bae3212b2d559e668a65bc8d0ebbb0befa263f10
Martin Jimenez <mar45pillacela@gmail.com>
2026-06-23 21:10:22 -0500

    Fix bug de saludo

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
-        System.out.println("Version: 1.0.0 lista para producción");
+        System.out.println("Version: 1.0.0 lista para producción con saludo correcto");
     }
 }
