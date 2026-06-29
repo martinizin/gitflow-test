2e4ed3e7ecc5e89b406deb2f246cea850d6bcef3
Martin Jimenez <mar45pillacela@gmail.com>
2026-06-28 22:23:01 -0500

    Agregar soporte multilingue

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+    Saludo saludo = new Saludo();
+    System.out.println(saludo.generarSaludo("Mundo", "en"));
+    System.out.println(saludo.generarSaludo("Mundo", "es"));
+    System.out.println(saludo.generarSaludo("Mundo", "fr"));
     System.out.println("***************");
     System.out.println("* JAVA PROJECT *");
     System.out.println("***************");
