2f5f7340117e569740e30f675f78dcecef33ab0c
Martin Jimenez <mar45pillacela@gmail.com>
2026-06-28 22:00:58 -0500

    Añadir función despedida

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Saludo saludo = new Saludo();
-        System.out.println(saludo.generarSaludo("Mundo"));
+        Despedida despedida = new Despedida();
+    System.out.println(despedida.generarDespedida("Mundo"));
     }
 }
